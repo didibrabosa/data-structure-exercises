8f34a42c1242a8ed98ed2c04a2d37c6746641a92
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2025-03-22 17:32:16 -0300

    feat: exercise that tells if a word is a palindrome

---
+++ is_palindrome.py
@@ -0,0 +1,12 @@
+word = "osso"
+print(word)
+
+word_4 = "o"
+word_3 = "s"
+word_2 = "s"
+word_1 = "o"
+
+word_inverted = word_4 + word_3 + word_2 + word_1
+
+if word_inverted == word:
+    print("its a palindrome")
